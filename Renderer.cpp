9a651400f24df844e2820ca4ade8ef3e90e29a59
isaak <isaak@laptop.(none)>
2011-12-30 18:42:46 +0100

    init

---
+++ Renderer.cpp
@@ -0,0 +1,20 @@
+/*
+ * Renderer.cpp
+ *
+ *  Created on: Dec 30, 2011
+ *      Author: isaak
+ */
+
+#include "Renderer.h"
+
+Renderer::Renderer() {
+}
+
+Renderer::~Renderer() {
+}
+
+int main() {
+
+
+	return 0;
+}
---
+++ Renderer.h
@@ -0,0 +1,17 @@
+/*
+ * Renderer.h
+ *
+ *  Created on: Dec 30, 2011
+ *      Author: isaak
+ */
+
+#ifndef RENDERER_H_
+#define RENDERER_H_
+
+class Renderer {
+public:
+	Renderer();
+	virtual ~Renderer();
+};
+
+#endif /* RENDERER_H_ */
