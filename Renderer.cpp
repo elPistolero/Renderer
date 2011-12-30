d4f7d6de3565bda1617f62c1668c5acdb0a8dbfb
isaak <isaak@laptop.(none)>
2011-12-30 22:21:08 +0100

    init setup of renderer

--- Renderer.cpp
+++ Renderer.cpp
@@ -7,14 +7,60 @@
 
 #include "Renderer.h"
 
-Renderer::Renderer() {
+Renderer::Renderer() : m_event(), m_projection(1.0) {
 }
 
 Renderer::~Renderer() {
 }
 
-int main() {
+bool Renderer::init() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+		return false;
 
+	if (!SDL_SetVideoMode(WIDTH, HEIGHT, BITSPERPIXEL, SDL_OPENGL))
+		return false;
+
+	if (!initGL())
+		return false;
+
+	return true;
+}
+
+bool Renderer::initGL() {
+	m_projection = glm::perspective(90.0f, (float)WIDTH/(float)HEIGHT, 1.0f, 1000.0f);
+
+	glClearColor(0.3, 0.3, 0.3, 1);
+
+	return true;
+}
+
+void Renderer::quit() {
+	SDL_Quit();
+}
+
+void Renderer::drawScreen() {
+	SDL_GL_SwapBuffers();
+}
+
+void Renderer::handleInput() {
+	while (SDL_PollEvent(&m_event)) {
+		if (m_event.type == SDL_QUIT)
+			quit();
+	}
+}
+
+void Renderer::mainLoop() {
+	while(true) {
+		handleInput();
+		drawScreen();
+	}
+}
+
+
+int main() {
+	Renderer renderer;
+	renderer.init();
+	renderer.mainLoop();
 
 	return 0;
 }
--- Renderer.h
+++ Renderer.h
@@ -8,10 +8,33 @@
 #ifndef RENDERER_H_
 #define RENDERER_H_
 
+#include "SceneGraph/SceneGraph.h"
+#include "SceneGraph/glm/glm/glm.hpp"
+#include "SceneGraph/glm/glm/gtc/matrix_transform.hpp"
+#include "SceneGraph/glm/glm/gtc/type_ptr.hpp"
+#include "ShaderHelper/ShaderHelper.hpp"
+#include "SDL/SDL.h"
+#include "SDL/SDL_opengl.h"
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+const int BITSPERPIXEL = 32;
+
 class Renderer {
 public:
 	Renderer();
 	virtual ~Renderer();
+	bool init();
+	void quit();
+	void drawScreen();
+	void mainLoop();
+
+private:
+	SDL_Event m_event;
+	glm::mat4 m_projection;
+
+	void handleInput();
+	bool initGL();
 };
 
 #endif /* RENDERER_H_ */
